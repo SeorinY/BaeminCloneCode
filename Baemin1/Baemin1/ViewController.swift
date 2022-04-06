edcc05063088d2b1c7d06da10e3ea043d8c98674
유서린 <81281190+SeorinY@users.noreply.github.com>
2022-04-06 20:17:53 +0900

    [FEA] : Create Project
    Create Storyboard, SwiftUI Project

---
+++ Baemin1/Baemin1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Baemin1
+//
+//  Created by Mac Book Pro on 2022/04/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
