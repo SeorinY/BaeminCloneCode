3325bd2109d4f78b5bd827ece7b86229e907fbbd
유서린 <81281190+SeorinY@users.noreply.github.com>
2022-05-31 13:12:32 +0900

    [FEA] : Create Search Porject
    Start Sprint 2

---
+++ Search_seorin/Search_seorin/Search_seorinApp.swift
@@ -0,0 +1,17 @@
+//
+//  Search_seorinApp.swift
+//  Search_seorin
+//
+//  Created by Mac Book Pro on 2022/05/31.
+//
+
+import SwiftUI
+
+@main
+struct Search_seorinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
